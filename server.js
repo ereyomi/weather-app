2d9fad5ee8e3d4aabb7e86a00a418b0d7dd3e637
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-06-13 20:05:47 +0100

    feat: added api key and setup app.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Require Express to run server and routes
 const express = require( 'express' );
 
@@ -30,4 +27,24 @@ const port = 8000;
 const server = app.listen( port, () => {
     console.log( "server running" );
     console.log( "running on localhost: ", port );
+} )
+
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = [
+    {
+    name: 'ereyomi'
+    },
+];
+
+
+// Callback function to complete GET '/all'
+app.get( '/api/getdata', (req, res) => {
+    res.send(projectData)
+})
+
+// Post Route
+app.post( '/api/postdata', ( req, res ) => {
+    projectData.push( req.body );
+    res.send( projectData );
 } )
--- website/app.js
+++ website/app.js
@@ -1,5 +1,91 @@
 /* Global Variables */
+const btn = document.querySelector( "#generate" ),
+    zipInput = document.querySelector( "#zip" ), 
+    feelingsInput = document.querySelector( "#feelings" );
+
+// Personal API Key for OpenWeatherMap API
+const apikey = '62f2b672194d4136da89832a175ba8ae';
+const endpoint = 'http://api.openweathermap.org/',
+
+//example = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c88b9d9f14854c57985e94d399cce307';
+// `http://api.openweathermap.org/data/2.5/weather?zip=${ zipcode},&appid=${ apikey }`;
+ 
+
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const generateDate = () => {
+    const d = new Date();
+    const newDate = `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
+    return newDate;
+}
+
+//
+
+/* Function to GET Web API Data*/
+const getWeatherData = async ( api ) => {
+    try
+    {
+        console.log(api)
+        await fetch( api )
+            .then( data => data.json() )
+            .then( data => console.log( data ) )
+    } catch ( error )
+    {
+        console.log( "error: ", error )
+    }
+}
+
+/* Function to POST data */
+const postData = async ( url = '', data = {} ) => {
+    console.log( data );
+    try
+    {
+        await fetch( url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+
+            body: JSON.stringify( data )
+        } ).then( data => data.json() )
+            .then( data => console.log( data ) )
+        
+    } catch ( error )
+    {
+        console.log( "error: ", error );
+
+    }
+}
+
+/* Function to GET Project Data */
+const getData = async () => {
+    try {
+        await fetch( '/api/getdata' )
+            .then( data => data.json() )
+            .then(data => console.log(data))
+    } catch (error) {
+        console.log("error: ", error)
+    }
+}
+
+/* Function called by event listener */
+const performProcess = () => {
+
+    zip = zipInput.value;
+    feelings = feelingsInput.value;
+
+    if ( zip !== '' && feelings !== '' )
+    {
+        const apiCall = `${ endpoint }/2.5/weather?zip=${ zipcode },${ countrycode }&appid=${ apikey }`;
+        getWeatherData( apiCall );
+    } else
+    {
+        let msg = '';
+        ( zip === '' ) ? msg = 'zip input is empty' : msg = 'feelings input is empty';
+    }
+
+}
+
+// Event listener to add function to existing HTML DOM element
+btn.addEventListener( 'click', performProcess );
